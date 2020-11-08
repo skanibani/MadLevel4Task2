586e284941b4cb121b1b6f81a4e79197982a50fa
Skander <skander@bibani.nl>
2020-11-08 23:03:32 +0100

    Extend Winner Enum with resource strings

--- app/src/main/java/com/example/madlevel4task2/model/Winner.kt
+++ app/src/main/java/com/example/madlevel4task2/model/Winner.kt
@@ -1,5 +1,18 @@
 package com.example.madlevel4task2.model
 
+import com.example.madlevel4task2.R
+
 enum class Winner {
-    PLAYER, COMPUTER, DRAW
+    PLAYER {
+        override fun getText(): Int = R.string.player_win
+
+    },
+    COMPUTER {
+        override fun getText(): Int = R.string.computer_win
+    },
+    DRAW {
+        override fun getText(): Int = R.string.draw
+    };
+
+    abstract fun getText(): Int
 }
